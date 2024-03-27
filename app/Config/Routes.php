184fc7ef223c492fc3758a15fd831abcd07fd763
OnlyCarry <onlycarrylan@gmail.com>
2024-03-27 15:01:34 -0600

    FIX Register

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,7 +10,7 @@ $routes->get('/', 'Home::index');
 service('auth')->routes($routes);
 
 $routes->group('api', ['namespace' => 'App\Controllers'], function ($routes) {
-    $routes->post('register', 'usersController::lastest');
+    $routes->post('register', 'usersController::create');
     $routes->get('lastestT', 'TournamentsController::data');
 });
 
